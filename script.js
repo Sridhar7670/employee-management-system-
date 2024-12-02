71e5515904e96930aa99b9e59732e5944b4a6b7f
Sridhar7670 <140085461+Sridhar7670@users.noreply.github.com>
2024-12-02 08:39:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+   const record_column = document.querySelector("#records-column");
+
+  
+
+const onSubmitForm = (event)=>{
+    event.preventDefault();
+    
+    const employee={
+            employeeId: event.target.employeeID.value,
+            name: event.target.name.value,
+            salary: event.target.Salary.value,
+            team: event.target.team.value,
+            companyName: event.target.companyName.value,
+            role: event.target.role.value
+
+    }
+    addemployeeData(employee);
+}
+
+function addemployeeData(employee){
+    let record = document.createElement("tr");
+    for(let key in employee)
+    {
+        const cell= document.createElement("td");
+        cell.innerText=employee[key];
+        record.append(cell);
+    }
+    let edit= document.createElement("span");
+    edit.className = "fa-regular fa-pen-to-square"; 
+    edit.style.padding="5px";
+    edit.addEventListener("click",editemployeeData)
+    
+
+    let dele= document.createElement("span");
+    dele.className = "fa-solid fa-trash";
+    record.append(edit,dele);
+    dele.style.padding="5px";
+     dele.addEventListener("click",deleeteemployeedata);
+     
+    record_column.append(record);
+
+    
+}
+
+const deleeteemployeedata=(event)=>{
+    // console.log(event.target.parentNode.parentnode);
+    console.log(event.target.parentNode);
+    event.target.parentNode.remove();
+    
+
+}
+
+const editemployeeData = (event) => {
+    const arr=event.target.parentNode.innerText.split('\t')
+    // console.log(arr)
+    for (let i = 0; i <arr .length - 2; i++) {
+        input[i].value=arr[i]
+        // console.log(input[i])
+    }
+
+   deleeteemployeedata(event);
+   alert("the data is deleted please update the existing form")
+   reset.disabled=true
+  
+}
+
+
